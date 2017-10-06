27446f3bf2604933fa3e3bc8d0467f3178f0ffe7
karthik78180 <karthik78180@gmail.com>
2017-10-06 15:43:27 -0500

    Added .jar file

---
+++ src/main/java/com/karthik/data/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.karthik.data.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+	@RequestMapping("/jenkin")
+	public String HelloJenkins() {
+		return "SpringBoot Application integrfation to jenkins";
+	}
+}
